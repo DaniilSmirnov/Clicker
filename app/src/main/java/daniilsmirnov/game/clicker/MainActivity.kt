3c7d9bc553806bfa6a2092d1ddccf9f356b18375
DaniilSmirnov <littlebigcrafting@gmail.com>
2018-05-16 21:20:37 +0300

    Сделаны функции под отображение названия пойманной мыши

--- app/src/main/java/daniilsmirnov/game/clicker/MainActivity.kt
+++ app/src/main/java/daniilsmirnov/game/clicker/MainActivity.kt
@@ -14,21 +14,30 @@ import kotlinx.android.synthetic.main.activity_main.view.*
 import android.widget.TextView
 import android.content.Context
 import android.graphics.Color
+import android.text.Html
 import android.util.TypedValue
 import android.widget.LinearLayout.LayoutParams
+import org.xmlpull.v1.XmlPullParser
+import android.R.xml
+import android.content.res.XmlResourceParser
+import org.xmlpull.v1.XmlPullParserException
+import android.app.Activity
+import java.io.IOException
+import java.util.*
 
 
 class MainActivity : AppCompatActivity() {
 
     //private val mContext: Context = applicationContext
 
-    var money: Int  = 0
+    var money: Int = 0
     var diamonds: Int = 0
     var power: Int = 100
     var energy: Int = 10
     var location = 1.1
 
     var i: Int = 0
+    val random = Random()
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
@@ -37,9 +46,9 @@ class MainActivity : AppCompatActivity() {
 
         //val context: Context = applicationContext
 
-        lateinit var mAdView : AdView
+        lateinit var mAdView: AdView
 
-        MobileAds.initialize(this,"ca-app-pub-7531773367714486~4707428901")
+        MobileAds.initialize(this, "ca-app-pub-7531773367714486~4707428901")
 
         val pipe = findViewById<ImageButton>(R.id.pipe)
         val money_view = findViewById<TextView>(R.id.money)
@@ -54,12 +63,12 @@ class MainActivity : AppCompatActivity() {
         var this_location_mice_cost: IntArray
 
         val energy_runnable = Runnable {
-            energy+=1
+            energy += 1
             Toast.makeText(this@MainActivity, "Thread runned", Toast.LENGTH_SHORT).show()
             Thread.sleep(60000)
         }
 
-        fun update_ui(){
+        fun update_ui() {
 
             energy_view.progress = energy
             energy_view_text.text = "$energy"
@@ -69,18 +78,19 @@ class MainActivity : AppCompatActivity() {
 
         }
 
-        fun add_event(){
+        fun add_event() {
 
-            mouse_info.setText("Test")
+            //mouse_info.setText((getString(R.values.locations.location)))
+            mouse_info.setText(getMousefromXML(this))
             mouse_icon_view.setImageResource(R.drawable.enemy1)
 
         }
 
 
-        fun init(){
+        fun init() {
 
-        //    val t = Thread(energy_runnable)
-          //  t.start()
+            //    val t = Thread(energy_runnable)
+            //  t.start()
             update_ui()
 
         }
@@ -89,17 +99,15 @@ class MainActivity : AppCompatActivity() {
 
         pipe.setOnClickListener {
 
-            if(energy > 0){
+            if (energy > 0) {
 
-                energy-=1
+                energy -= 1
                 energy_view_text.text = "$energy"
                 energy_view.progress = energy
 
                 add_event()
 
-            }
-            else
-            {
+            } else {
 
                 Toast.makeText(this@MainActivity, "Not enough energy", Toast.LENGTH_SHORT).show()
 
@@ -109,11 +117,35 @@ class MainActivity : AppCompatActivity() {
 
         }
 
-        event_card.setOnClickListener{
+        event_card.setOnClickListener {
 
-            val intent_events = Intent( this, ScrollingActivity::class.java)
+            val intent_events = Intent(this, ScrollingActivity::class.java)
             startActivity(intent_events)
         }
 
     }
+
+    private fun rand(from: Int, to: Int) : Int {
+        return random.nextInt(to - from) + from
+    }
+
+    @Throws(XmlPullParserException::class, IOException::class)
+    private fun getMousefromXML(activity: Activity): String {
+        val stringBuilder = StringBuilder()
+        val res = activity.resources
+        val xmlResourceParser = res.getXml(R.xml.locations)
+        xmlResourceParser.next()
+        var eventType = xmlResourceParser.eventType
+        while (eventType != XmlPullParser.END_DOCUMENT) {
+            stringBuilder.append(xmlResourceParser.text)
+            i=rand(1,2)
+            if (eventType == XmlPullParser.START_TAG
+                    && xmlResourceParser.getName().equals("mice"+"$i")) {
+                eventType = xmlResourceParser.next()
+            }
+        }
+
+        return stringBuilder.toString()
+    }
+
 }
